f0d377f50e721de585151177328577e78d905f1c
MasonEZ <ezekiel.m.mason@gmail.com>
2020-10-30 17:07:17 +0000

    friday_commit

--- starloop.py
+++ starloop.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 #1 to 5 star * pattern for loop
 
-# List of Items strings
-star = ["*"]
+def zeke()
+    # List of Items strings
+    star = ["*"]
 
-# turning list into duplicate output.
-for x in star:
-    for y in range(1,6):
-        print(y*x)
+    # turning list into duplicate output.
+    for x in star:
+        for y in range(1,6):
+            print(y*x)
 
-#reversing output
-for x in star:
-    for y in reversed(range(1,6)):
-        print(y*x)
-print()
+    #reversing output
+    for x in star:
+        for y in reversed(range(1,6)):
+            print(y*x)
+    print()
+zeke()
+
