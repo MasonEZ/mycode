3926434270a0576142cab12a3037ddd7fe22442c
MasonEZ <73487497+MasonEZ@users.noreply.github.com>
2020-11-04 11:09:25 -0800

    Add files via upload

---
+++ flowchart.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# warm up
+import time
+
+# Flowchart start
+#First Question
+def q1():
+    print("Its a normal day on the I-5 in Washington State. You are driving to Seattle from Tacoma.")
+    print("The car in front of you turns on their turn signal to move into the passing lane. Do you...")
+    print("1. SLAM ON BRAKES!!!\n")
+    time.sleep(3)
+    print("2. continue to tweet and drive\n")
+    ans= input("1 or 2")
+    if ans == "1":
+        print("")
+        print("WoW! What an @z$H013! We almost died!")
+    if ans == "2":
+        print("")
+        print("You see a funny meme, put your phone down, and continue driving.")
+#    elif ans != ""
+    elif ans == "q":
+        print("Leave then... My mother was right about you.")
+        exit()
+# Second Question
+def q2():
+    print("So, you've survived 5 minutes of others driving on the same road as you?")
+    print("You see a Prius driving significantly slower than you. Do You... ")
+    print("1. safely use the passing lane and then move back over to the slow lane.\n")
+    print("2. slow down to whatever speed they're driving at and just drive slow to work.\n")
+
+    ans ""
+
+
+if __name__ == '__main__':
