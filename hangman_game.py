ffdfa2b3f83784a38fefbb85c749ac7ba249234d
MasonEZ <73487497+MasonEZ@users.noreply.github.com>
2020-11-05 22:35:11 -0800

    Add files via upload

--- hangman_game.py
+++ hangman_game.py
@@ -2,7 +2,10 @@
 # Hangman game. Enjoy!
 import random
 import sys
-from typing import List
+import time
+
+
+# from typing import List
 
 words = ["harmony", "umbrella", "window", "kislev", "spooky", "season", "mail", "fig",
          "laptop", "dog", "band", "lemon", "beautiful", "handle", "syrup"]
@@ -13,17 +16,7 @@ answer_length = len(answer)
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 letter_storage = []
 
-# Utility
-
-def print_word_to_guess(letters: List) -> None:
-    print("Word to guess: {0}".format(" ".join(letters)))
-
-
-def print_guesses_taken(current: int, total: int) -> None:
-    print("Attempts remaining: {0}/{1}.".format(current, total))
-
-# Functions
-
+# Welcome message
 def beginning() -> None:
     print("Hello!\n")
     while True:
@@ -32,15 +25,25 @@ def beginning() -> None:
             print("White SPACES!!!")
         else:
             break
+# Utility
 
+def print_word_to_guess(letters: answer_length) -> None:
+    print("Word to guess: {0}".format(" ".join(letters)))
+
+
+def print_guesses_taken(current: int, total: int) -> None:
+    print("Attempts remaining: {0}/{1}.".format(current, total))
+
+
+# Functions
 
 def ask_user_to_play() -> None:
     print("Wanna play a game?!?!\n")
     while True:
-        gameChoice = input("You do or you do not. There is no maybe!\n").upper()
-        if gameChoice == "YES" or gameChoice == "Y":
+        gamechoice = input("You do or you do not. There is no maybe!\n").upper()
+        if gamechoice == "YES" or gamechoice == "Y":
             break
-        elif gameChoice == "NO" or gameChoice == "N":
+        elif gamechoice == "NO" or gamechoice == "N":
             sys.exit("That's a shame! Have a nice day")
         else:
             print("Please Answer only Yes or No")
@@ -59,36 +62,45 @@ def prepare_secret_word() -> None:
 def guessing() -> None:
     # Game loop
     guess_taken = 1
-    attempts = 10
+    attempts = 5
     print_guesses_taken(guess_taken, attempts)
 
-    while guess_taken < attempts:
+    while guess_taken <= attempts:
         guess = input("Pick a letter\n").lower()
         # Checking input
         if not guess in alphabet:
-            print("Enter a letter from a-z")
+            print("Enter a letter from a-z\n\t")
             #  checking if letter has been already used
         elif guess in letter_storage:
-            print("You have already guessed that letter!")
+            print("You have already guessed that letter!\n\t")
         else:
             letter_storage.append(guess)
             if guess in answer:
                 print("Correct, choose another!")
                 for i in range(0, answer_length):
                     if answer[i] == guess:
-                        answer[i] = guess
+                        answer[i] == guess
                 print_word_to_guess(guess)
                 print_guesses_taken(guess_taken, attempts)
-                if not '-' in guess:
-                    print("You won!")
-                    print("You Lose!")
-                    break
+                return
+                exec(beginning)
+
             else:
                 print("Ding Dong you are Rong. Try Again!")
                 guess_taken += 1
                 print_guesses_taken(guess_taken, attempts)
-                if guess_taken == 10:
+                if guess_taken == 5:
                     print(" I'm sorry, but no. The secret word was {0}".format(answer))
+                    time.sleep(3)
+                    print("Their blood is on your hands. Now wash up and then come back for more FUN and EXCITEMENT")
+                    time.sleep(5)
+                    exec(ask_user_to_play)
+                else:
+                    continue
+
+
+
+guessing()
 
 
 if __name__ == "__main__":
