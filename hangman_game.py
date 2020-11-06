c3d3e770b74351d36ddc8132623b373911c53706
MasonEZ <73487497+MasonEZ@users.noreply.github.com>
2020-11-06 13:35:03 -0800

    Add files via upload

--- hangman_game.py
+++ hangman_game.py
@@ -15,10 +15,12 @@ answer = random.choice(words)
 answer_length = len(answer)
 guess = []
 right = ['_'] * len(answer)
-wrong = ['_'] * len(answer)
+wrong = []
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 letter_storage = []
+
+print("=======================================")
 
 # Welcome message
 def beginning() -> None:
@@ -37,6 +39,7 @@ def print_word_to_guess(letters: answer_length) -> None:
 
 def print_guesses_taken(current: int, total: int) -> None:
     print("Attempts remaining: {0}/{1}.".format(current, total))
+
 
 
 # Functions
@@ -62,6 +65,22 @@ def prepare_secret_word() -> None:
     print("You can only enter 1 letter from a to z\n\n")
     print_word_to_guess(guess)
 
+# shows correct and incorrect letters
+#Prints right letters with _'s
+def right_letter():
+    for i in right:
+        print(i, ' ', end = "")
+    print()
+
+#wrong letters
+def wrong_letter():
+    print("Wrong letters:", end = "")
+    for i in wrong:
+        print(i,' ',end = "")
+    print()
+# Continues to display dashes?
+for i in range(len(answer)):
+    print('_',' ', end = "")
 
 def guessing() -> None:
     # Game loop
@@ -69,38 +88,49 @@ def guessing() -> None:
     attempts = 5
     print_guesses_taken(guess_taken, attempts)
 
-    while guess_taken != attempts: #Changed from >=
+    while guess_taken != attempts:
+        print("===============================")
         guess = input("Pick a letter\n").lower()
         # Checking input
         if not guess in alphabet:
             print("Enter a letter from a-z\n\t")
             #  checking if letter has been already used
+
         elif guess in letter_storage:
             print("You have already guessed that letter!\n\t")
+
+        elif guess in answer:
+            #letter_storage.append(guess)
+            right.append(guess)
+            print("Correct!\n")
+            time.sleep(1)
+            for i in range(0, answer_length):
+                if answer[i] == guess:
+                    print("Choose another letter:\N")
+
+            print_word_to_guess(guess)  #(guess.format(answer))?
+            print_guesses_taken(guess_taken, attempts)
+
         else:
-            letter_storage.append(guess)
-            if guess in answer:
-                right.append(guess)
-                print("Correct, choose another!")
-                for i in range(0, answer_length):
-                    if answer[i] == guess:
-                        answer[i] == guess
-                print_word_to_guess(guess)  #(guess.format(answer))?
-                print_guesses_taken(guess_taken, attempts)
+            print("Ding Dong you are Rong. Try Again!")
+            wrong.append(guess)
+            guess_taken += 1
+            print_guesses_taken(guess_taken, attempts)
+            if guess_taken == 5:
+                print(" I'm sorry, but no. The secret word was {0}".format(answer))
+                time.sleep(3)
+                print("Their blood is on your hands. Now wash up and then come back for more FUN and EXCITEMENT")
+                time.sleep(5)
+                print('   ', '------')
+                print('   ', '|    |')
+                print('   ', '|    O')
+                print('   ', '|   -|-')
+                print('   ', '|    | ')
+                print('   ', '|   / \\')
+                print('------------')
 
             else:
-                print("Ding Dong you are Rong. Try Again!")
-                wrong.append(guess)
-                guess_taken += 1
-                print_guesses_taken(guess_taken, attempts)
-                if guess_taken == 5:
-                    print(" I'm sorry, but no. The secret word was {0}".format(answer))
-                    time.sleep(3)
-                    print("Their blood is on your hands. Now wash up and then come back for more FUN and EXCITEMENT")
-                    time.sleep(5)
-                    exec(ask_user_to_play)
-                else:
-                    continue
+                continue
 
 
 
@@ -112,3 +142,6 @@ if __name__ == "__main__":
     ask_user_to_play()
     prepare_secret_word()
     guessing()
+    wrong_letter()
+    right_letter()
+
